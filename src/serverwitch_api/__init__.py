6f773e3c84f6db1776fddb83da4f72e9450c0dae
Vi Retault <g33kex@pm.me>
2024-01-30 21:27:27 -0500

    added documentation and improved logging

--- src/serverwitch_api/__init__.py
+++ src/serverwitch_api/__init__.py
@@ -1,8 +1,11 @@
-from dataclasses import dataclass
+"""This is the main file of the Server Witch API.
+It describes the endpoints of the API."""
+from dataclasses import dataclass, asdict
 import importlib.metadata
 import secrets
 import asyncio
 import logging
+import json
 from typing import Tuple
 
 from quart import Quart, websocket, request
@@ -11,97 +14,121 @@ from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from serverwitch_api.broker import SessionBroker, SessionDoesNotExist, ClientRequest, ClientResponse, ClientError
 
+"""Timeout waiting for a response from the client.
+Currently, ChatGPT will timeout after 45 seconds if it didn't receive a response.
+We want to timeout before that to provide it with more context about the reasons of the timeout."""
 TIMEOUT: int = 40
-TRUSTED_HOSTS: list[str] = ["127.0.0.1"]
+"""Log Level. Setting log level to INFO will log every request."""
 LOG_LEVEL: int = logging.INFO
+"""Who to trust with X-Forwarded-For Headers"""
+TRUSTED_HOSTS: list[str] = ["127.0.0.1"]
 
 app = Quart(__name__)
 QuartSchema(app)
 app.asgi_app = ProxyHeadersMiddleware(app.asgi_app, trusted_hosts=TRUSTED_HOSTS)
 
+FORMAT = "%(process)s %(thread)s: %(message)s"
 app.logger.setLevel(LOG_LEVEL)
+app.logger.handlers[0].setFormatter(logging.Formatter("[%(created)f] [%(levelname)s] %(message)s"))
 
 broker = SessionBroker()
 
+"""Run the app"""
 def run() -> None:
     app.run()
 
+"""API Status response"""
 @dataclass
 class Status:
     status: str
     version: str
 
+
+"""Session information sent into the websocket"""
 @dataclass
 class Session:
     session_id: str
 
+"""Command request"""
 @dataclass
 class Command:
     session_id: str
     command: str
 
+"""Read request"""
 @dataclass
 class Read:
     session_id: str
     path: str
 
+"""Write request"""
 @dataclass
 class Write:
     session_id: str
     path: str
     content: str
 
+"""Response to a command request"""
 @dataclass
 class CommandResponse:
     return_code: int
     stdout: str
     stderr: str
 
+"""Response to a read request"""
 @dataclass
 class ReadResponse:
     content: str
 
+"""Response to a write request"""
 @dataclass
 class WriteResponse:
     size: int
 
+"""Response in case of an error"""
 @dataclass
 class ErrorResponse:
     error: str
 
+"""Receive the client responses through the websocket"""
+async def _receive(session_id) -> None:
+    while True:
+        response = await websocket.receive_as(ClientResponse) # type: ignore
+        app.logger.info(f"{websocket.remote_addr} - RESPONSE - {session_id} {json.dumps(asdict(response))}")
+        await broker.receive_response(session_id, response)
+
+"""Fetch the status of the API"""
 @app.get("/status") # type: ignore
 @validate_response(Status)
 async def status() -> Status:
     return Status(status="OK", version=importlib.metadata.version('serverwitch-api'))
 
-async def _receive(session_id) -> None:
-    while True:
-        response = await websocket.receive_as(ClientResponse) # type: ignore
-        await broker.receive_response(session_id, response)
-
+"""Client requests a new session.
+Calling this endpoints initiates a websocket used to send requests
+to the client and receive the responses."""
 @app.websocket('/session')
 async def session_handler():
     session_id = secrets.token_hex()
-    app.logger.info(f"Creating new session for {websocket.remote_addr}")
+    app.logger.info(f"{websocket.remote_addr} - SESSION - {session_id}")
     await websocket.send_as(Session(session_id=session_id), Session) # type: ignore
 
     task = None
     try:
         task = asyncio.ensure_future(_receive(session_id))
         async for request in broker.subscribe(session_id):
-            app.logger.info(f"Sending request: {request}")
+            app.logger.info(f"{websocket.remote_addr} - REQUEST - {session_id} {json.dumps(asdict(request))}")
             await websocket.send_as(request, ClientRequest) # type: ignore
     finally:
         if task is not None:
             task.cancel()
             await task
 
+"""Execute a command on the client"""
 @app.post('/command') # type: ignore
 @validate_request(Command)
 @validate_response(CommandResponse, 200)
 @validate_response(ErrorResponse, 500)
 async def command(data: Command) -> Tuple[CommandResponse | ErrorResponse, int]:
-    app.logger.info(f"Receiving command from {request.remote_addr}")
     try:
         response = CommandResponse(**await broker.send_request(
             data.session_id,
@@ -109,18 +136,19 @@ async def command(data: Command) -> Tuple[CommandResponse | ErrorResponse, int]:
             timeout=TIMEOUT))
         return response, 200
     except SessionDoesNotExist:
+        app.logger.warning(f"{request.remote_addr} - INVALID SESSION ID - {repr(data.session_id)}")
         return ErrorResponse('Session does not exist.'), 500
     except ClientError as e:
         return ErrorResponse(e.message), 500
     except asyncio.TimeoutError:
         return ErrorResponse('Timeout when waiting for client.'), 500
 
+"""Read a file from the client"""
 @app.post('/read') # type: ignore
 @validate_request(Read)
 @validate_response(ReadResponse, 200)
 @validate_response(ErrorResponse, 500)
 async def read(data: Read) -> Tuple[ReadResponse | ErrorResponse, int]:
-    app.logger.info(f"Received read from {request.remote_addr}")
     try:
         response = ReadResponse(**await broker.send_request(
             data.session_id,
@@ -128,18 +156,19 @@ async def read(data: Read) -> Tuple[ReadResponse | ErrorResponse, int]:
             timeout=TIMEOUT))
         return response, 200
     except SessionDoesNotExist:
+        app.logger.warning(f"{request.remote_addr} - INVALID SESSION ID - {repr(data.session_id)}")
         return ErrorResponse('Session does not exist.'), 500
     except ClientError as e:
         return ErrorResponse(e.message), 500
     except asyncio.TimeoutError:
         return ErrorResponse('Timeout when waiting for client.'), 500
 
+"""Write a file on the client"""
 @app.post('/write') # type: ignore
 @validate_request(Write)
 @validate_response(WriteResponse, 200)
 @validate_response(ErrorResponse, 500)
 async def write(data: Write) -> Tuple[WriteResponse | ErrorResponse, int]:
-    app.logger.info(f"Received write from {request.remote_addr}")
     try:
         response = WriteResponse(**await broker.send_request(
             data.session_id,
@@ -147,9 +176,9 @@ async def write(data: Write) -> Tuple[WriteResponse | ErrorResponse, int]:
             timeout=TIMEOUT))
         return response, 200
     except SessionDoesNotExist:
+        app.logger.warning(f"{request.remote_addr} - INVALID SESSION ID - {repr(data.session_id)}")
         return ErrorResponse('Session does not exist.'), 500
     except ClientError as e:
         return ErrorResponse(e.message), 500
     except asyncio.TimeoutError:
         return ErrorResponse('Timeout when waiting for client.'), 500
-
--- src/serverwitch_api/broker.py
+++ src/serverwitch_api/broker.py
@@ -3,37 +3,47 @@ import uuid
 from typing import AsyncGenerator, Dict, Tuple, Any
 from dataclasses import dataclass
 
+"""Error when a requesting a session id that doesn't exist"""
 class SessionDoesNotExist(Exception):
     pass
 
+"""Error when creating a session with a session id that already exists"""
 class SessionAlreadyExists(Exception):
     pass
 
+"""Error when the client returned an error"""
 class ClientError(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
 
+"""Request sent to the client"""
 @dataclass
 class ClientRequest:
     request_id: str
     data: Any 
 
+"""Response received from the client"""
 @dataclass
 class ClientResponse:
     request_id: str
     error: bool
     data: Any
 
+"""This broker routes the requests to the clients and waits for the responses"""
 class SessionBroker:
     def __init__(self):
+        """Dictionary of session id -> queue of messages waiting to be sent to the client"""
         self.sessions: Dict[str, asyncio.Queue] = {}
+        """Dictionary of (session id, request id) -> future of a client response to that particular request"""
         self.pending_responses: Dict[Tuple[str, str], asyncio.Future] = {}
 
+    """Send a request to the client with the session id"""
     async def send_request(self, session_id: str, data: Any, timeout: int = 60) -> Any:
         if session_id not in self.sessions:
             raise SessionDoesNotExist()
 
+        # Generate an id for the request that will be sent back by the client alongside the response
         request_id = str(uuid.uuid4())
         future = asyncio.get_event_loop().create_future()
         self.pending_responses[(session_id, request_id)] = future
@@ -51,6 +61,8 @@ class SessionBroker:
             if (session_id, request_id) in self.pending_responses:
                 del self.pending_responses[(session_id, request_id)]
             
+    """Receive client responses from a particular session.
+    This resolves the response future."""
     async def receive_response(self, session_id: str, response: ClientResponse) -> None:
         if (session_id, response.request_id) in self.pending_responses:
             future = self.pending_responses.pop((session_id, response.request_id))
@@ -60,7 +72,8 @@ class SessionBroker:
                 else:
                     future.set_result(response.data)
 
-    async def subscribe(self, session_id: str) -> AsyncGenerator[str, None]:
+    """Add a new client with a session id and yield the requests to send"""
+    async def subscribe(self, session_id: str) -> AsyncGenerator[ClientRequest, None]:
         if session_id in self.sessions:
             raise SessionAlreadyExists()
 
